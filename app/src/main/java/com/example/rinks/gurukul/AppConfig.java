d7f04015bb48f0b18d5e614d152989e2e965b57e
RInksG <rgorasia82@gmail.com>
2017-05-24 18:36:07 +0530

    Gurukul project
    Gurukul

---
+++ app/src/main/java/com/example/rinks/gurukul/AppConfig.java
@@ -0,0 +1,9 @@
+package com.example.rinks.gurukul;
+
+class AppConfig {
+	// Server user login url
+	static String URL_LOGIN = "https://rgorasia82.000webhostapp.com/login.php";
+
+	// Server user register url
+	static String URL_REGISTER = "https://rgorasia82.000webhostapp.com/register.php";
+}
